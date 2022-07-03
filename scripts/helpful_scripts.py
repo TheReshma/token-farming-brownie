e2ab1a00ce1c1c9dd2785660cd5dde896fa3c643
Reshma <reshmashaik3011@gmail.com>
2022-07-03 15:53:31 +0530

    feat: Mock contracts & helper scripts

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,60 @@
+from brownie import (
+    network,
+    accounts,
+    config,
+    LinkToken,
+    MockV3Aggregator,
+    MockWETH,
+    MockDAI,
+    Contract,
+)
+
+LOCAL_BLOCKCHAIN_ENV = ["mainnet-fork-dev", "development"]
+DECIMALS = 18
+INITIAL_VALUE = 2000000000000000000000
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "dai_usd_price_feed": MockV3Aggregator,
+    "fau_token": MockDAI,
+    "weth_token": MockWETH,
+}
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENV :
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    return accounts.add(config["wallets"]["from_key"])
+
+def get_contract(contract_name):
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENV:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        try:
+            contract_address = config["networks"][network.show_active()][contract_name]
+            contract = Contract.from_abi( contract_type._name, contract_address, contract_type.abi)
+        except KeyError:
+            print(f"Theres some error here in getting contracts on {network.show_active()}")
+
+def deploy_mocks( decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    print(f"The active network is{network.show_active()}")
+    account = get_account()
+    link_token = LinkToken.deploy({"from": account})
+    mock_price_feed = MockV3Aggregator.deploy({"from": account})
+    mock_dai = MockDAI.deploy({"from": account})
+    mock_weth = MockWETH.deploy({"from": account})
+    print(f"Link deployed on {link_token.address}, V3Aggregator on {mock_price_feed.address}")
+    print(f"DAI on {mock_dai.address}, WETH on {mock_weth.address}")
+
+
+
+
+
+
+
